56a815f484da5bd7c1a4195ae06436509510c1f8
Varun Kumar <varun.kumar@dineout.co.in>
2019-03-06 13:24:50 +0530

    test commit
    test commit

--- WKWebViewController/WKWebViewController.swift
+++ WKWebViewController/WKWebViewController.swift
@@ -25,7 +25,7 @@ class WKWebViewController: UIViewController, WKUIDelegate {
         override func viewDidLoad() {
             super.viewDidLoad()
             
-            let myURL = URL(string:webRequestUrl ?? "https://www.apple.com")
+            let myURL = URL(string:webRequestUrl ?? "https://www.google.com")
             let myRequest = URLRequest(url: myURL!)
             webView.load(myRequest)
         }
